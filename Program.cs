04607879817496edbc54664b9da42a3de1a3d480
josh-thornton <frocious@gmail.com>
2019-07-29 20:47:43 -0600

    strings, ints, bools

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrimitiveTypes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Console.WriteLine("Welcome to Dotnet 101!");
+      Console.WriteLine("What is your name?");
+      string name = Console.ReadLine();
+      Console.WriteLine($"Welcome to class, {name}!");
+      Console.WriteLine($"There are {name.Length} letters in your name!");
+      Console.WriteLine("How old are you?");
+      string strAge = Console.ReadLine();
+      Int32.TryParse(strAge, out int age);
+      Console.WriteLine($"In ten years {name} will be {age + 10}");
+      //   int age = 33;
+      //   decimal bankAccount = 10.75m;
+      //   float sisterAge = 23f;
+      //   bool imTrue = true;
+    }
+  }
+}
